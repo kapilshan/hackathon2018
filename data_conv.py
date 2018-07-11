8bddaac1c13cfac55055f7c2e28393e0f4af50f2
Alagappan <kapil@Alagappans-MacBook-Pro.local>
2018-07-10 20:38:45 -0500

    k

--- data_conv.py
+++
@@ -1,7 +0,0 @@
-from pandas import Series
-series = Series.from_csv('datarev.csv', header=0)
-split_point = len(series) - 6
-dataset, validation = series[0:split_point], series[split_point:]
-print('Dataset %d, Validation %d' % (len(dataset), len(validation)))
-dataset.to_csv('dataset.csv')
-validation.to_csv('validation.csv')
